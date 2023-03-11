9f9e850218e4f1611d6a02bc2c0327399f20d236
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-03-11 21:14:00 +0300

    added the methods to add items to the front and back of the array

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Write your solution here!
 const cats=["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(name){
+cats.push('Ralph');
+}
+function destructivelyPrependCat(name){
+    cats.unshift('Bob');
+}
